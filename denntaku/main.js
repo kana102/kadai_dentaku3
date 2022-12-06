7e0ffed68c3472157d710d77baf0a55019a18c31
kana102 <114126791+kana102@users.noreply.github.com>
2022-12-06 21:19:14 +0900

    Add files via upload

--- denntaku/main.js
+++ denntaku/main.js
@@ -1,28 +1,26 @@
-// データ
 var result = "";
-// =で計算したかどうか
 var is_calc = false;
 
-// 初期表示
 window.onload = function () {
   result = document.getElementById('result');
 };
 
-// Cキー押下
 function c_click(){
   result.value = "0";
   is_calc = false;
 }
 
-// 数字キー押下
+
 function num_click(val){
-  if(is_calc)  result.value = "0";
+  if(is_calc)  result.value = "0","00";
   is_calc = false;  
 
   if(result.value =="0" && val == "0"){
     result.value = "0";
   }else if(result.value == "0" && val == "."){
     result.value = "0.";
+  }else if(result.value == "0" && val == "00"){
+    result.value = "0";
   }else if(result.value == "0"){
     result.value = val;
   }else{
@@ -30,7 +28,8 @@ function num_click(val){
   }
 }
 
-// 演算子キー押下
+
+
 function ope_click(val){
   if(is_calc)  is_calc = false;
   
@@ -41,7 +40,8 @@ function ope_click(val){
   }
 }
 
-// =キークリック
+
+
 function equal_click(){
   if(is_ope_last())  result.value = result.value.slice(0, -1);
 
@@ -54,7 +54,7 @@ function equal_click(){
   }
 }
 
-// 入力されている値が演算子かどうか
+
 function is_ope_last(){
-  return ["+","-","×","÷"].includes(result.value.toString().slice(-1));
+  return ["+","-","*","/",".","00"].includes(result.value.toString().slice(-1));
 }
